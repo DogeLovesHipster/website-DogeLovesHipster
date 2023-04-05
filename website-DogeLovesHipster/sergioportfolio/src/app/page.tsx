0fd7918781f671ad30a3deed5bf2a9b91b99f41f
Sergio Mendoza <mendozas1@sou.edu>
2023-04-05 10:16:05 -0700

    Personal Changes

--- website-DogeLovesHipster/sergioportfolio/src/app/page.tsx
+++ website-DogeLovesHipster/sergioportfolio/src/app/page.tsx
@@ -1,4 +1,3 @@
-import React from 'react'
 import Image from 'next/image'
 import { Inter } from 'next/font/google'
 import styles from './page.module.css'
@@ -7,8 +6,12 @@ const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
   return (
-    <header>
-      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusantium culpa ipsam possimus consectetur, magni, at ex sed id excepturi magnam ea, aliquid autem facere hic accusamus suscipit libero nostrum laudantium?</p>
+    <main className={styles.main}>
+      <header className={styles.head}>
+      <h1>Sergio's Port</h1>
     </header>
-  );
+      <div className={styles.description}>
+      </div>
+    </main>
+  )
 }
