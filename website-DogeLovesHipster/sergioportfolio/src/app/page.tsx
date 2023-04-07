0848767c2dad4e32190d0a99dd5397c2e7a0c437
Sergio <shadow41402@gmail.com>
2023-04-06 23:12:33 -0700

    Added: Basic Structuring

--- website-DogeLovesHipster/sergioportfolio/src/app/page.tsx
+++ website-DogeLovesHipster/sergioportfolio/src/app/page.tsx
@@ -6,12 +6,20 @@ const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
   return (
+    <body>
     <main className={styles.main}>
       <header className={styles.head}>
-      <h1>Sergio's Port</h1>
-    </header>
+        <h1>Sergio's Port</h1>
+        <h2>Hi, I'm Sergio. I'm a cool guy.</h2>
+        <nav>
+        </nav>
+      </header>
       <div className={styles.description}>
       </div>
+      <footer>
+        <p>Footer Goes here</p>
+      </footer>
     </main>
+    </body>
   )
 }
