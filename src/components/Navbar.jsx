b029f33522ab95512a47046176bdb3a6367e7fd6
Sergio <shadow41402@gmail.com>
2023-11-15 00:40:51 -0800

    Changed to new logo

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -42,7 +42,7 @@ const Navbar = () => {
             window.scrollTo(0, 0);
           }}
         >
-          <img src={logo} alt='logo' className='w-9 h-9 object-contain' />
+          <img src={logo} alt='logo' className='w-20 h-15 object-contain' />
           <p className='text-white text-[18px] font-bold cursor-pointer flex '>
             Sergio &nbsp;
             <span className='sm:block hidden'> | Don't Believe the Hype </span>
