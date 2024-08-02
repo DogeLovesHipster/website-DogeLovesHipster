fb28352024f61c55c5157bbf1e495edfebfbc0d1
Sergio <shadow41402@gmail.com>
2024-08-02 01:52:06 -0700

    Attempted index file fixes

--- server/index.js
+++ server/index.js
@@ -1,16 +1,24 @@
-import express from 'express';
-import path from 'path';
-import { fileURLToPath } from 'url';
-import { dirname } from 'path';
+import express from "express";
+import path from "path";
+import { fileURLToPath } from "url";
+import { dirname } from "path";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
 const app = express();
 
-app.use('/build', express.static(path.join(__dirname, 'public/build')));
-app.use('/static', express.static(path.join(__dirname, '../public/build/static')));
+app.use("/build", express.static(path.join(__dirname, "public/build")));
+app.use(
+  "/static",
+  express.static(path.join(__dirname, "../public/build/static"))
+);
+
+// Catch-all route to serve index.html for client-side routing
+app.get("/*", (req, res) => {
+  res.sendFile(path.join(__dirname, "public/build", "index.html"));
+});
 
 app.listen(3000, () => {
-    console.log('Server is running on port 3000');
+  console.log("Server is running on port 3000");
 });
